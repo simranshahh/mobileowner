db4c697b9dd44597788644b4bfd0fae8a3f86f34
simranshahh <simransah314@gmail.com>
2023-07-26 10:58:49 +0545

    page view

---
+++ lib/auth/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('data'),
+    );
+  }
+}
