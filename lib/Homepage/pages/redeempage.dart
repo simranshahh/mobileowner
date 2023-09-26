66d08fad841d7404838bb86da5761ae8aa8acfc9
simranshahh <simransah314@gmail.com>
2023-09-26 14:45:35 +0545

    sorting

--- lib/Homepage/pages/redeempage.dart
+++ lib/Homepage/pages/redeempage.dart
@@ -13,6 +13,15 @@ class ReedemPage extends StatefulWidget {
 
 class _ReedemPageState extends State<ReedemPage> {
   String _selectedOption = 'Option 1';
+  bool showRecentData = false;
+
+  void toggleData() {
+    setState(() {
+      showRecentData = !showRecentData;
+    });
+  }
+
+  bool _isFilterActive = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,122 +43,17 @@ class _ReedemPageState extends State<ReedemPage> {
               ),
               IconButton(
                 // key: _filterButtonKey,
-                onPressed: () {},
+                onPressed: () {
+                  toggleData();
+                  if (showRecentData) {
+                    RecentDataWidget();
+                  } else {
+                    NextDataWidget();
+                  }
+                },
                 icon: const Icon(Icons.filter_list),
               ),
             ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            onChanged: (searchTerm) {
-              // _performSearch(searchTerm);
-            },
-            cursorColor: ColorConstant.primary,
-            decoration: InputDecoration(
-              disabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(width: 1, color: ColorConstant.paleblue),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(width: 1, color: ColorConstant.paleblue),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              hintText: "Search",
-              filled: false,
-              fillColor: const Color(0xfff1f1f1),
-              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: BorderSide(color: ColorConstant.paleblue),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(width: 1, color: Colors.grey),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              suffixIcon: Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 12,
-        ),
-        Expanded(
-          child: SizedBox(
-            height: height * 0.5,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              elevation: 3,
-              shadowColor: Colors.black,
-              clipBehavior: Clip.antiAlias,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Image.asset('assets/cafe1.png'),
-                    SizedBox(
-                        // width: 8,
-                        ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(' hi',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall!
-                                .copyWith(color: ColorConstant.primary)),
-                        Text('hi',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: ColorConstant.paleblue))
-                      ],
-                    ),
-                    Spacer(),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('Reward Points',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                    fontWeight: FontWeight.w500,
-                                    color: ColorConstant.paleblue)),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/coin.png',
-                              alignment: Alignment.center,
-                            ),
-                            // SizedBox(
-                            //   width: width * 0.01,
-                            // ),
-                            Text('hi',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(color: ColorConstant.primary))
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
           ),
         ),
       ],
@@ -157,44 +61,133 @@ class _ReedemPageState extends State<ReedemPage> {
   }
 }
 
-  //   void _showPopupMenu() async {
-  //   FocusScope.of(context).unfocus();
-  //   final RenderBox overlay =
-  //       _filterButtonKey.currentContext!.findRenderObject() as RenderBox;
+class NextDataWidget extends StatelessWidget {
+  const NextDataWidget({super.key});
 
-  //   final position = RelativeRect.fromRect(
-  //     Rect.fromPoints(
-  //       overlay.localToGlobal(Offset(0, overlay.size.height + 150),
-  //           ancestor: context.findRenderObject()!),
-  //       overlay.localToGlobal(
-  //           Offset(overlay.size.width, overlay.size.height + 200),
-  //           ancestor: context.findRenderObject()!),
-  //     ),
-  //     Offset.zero & MediaQuery.of(context).size,
-  //   );
-  //   String? selectedOption = await showMenu(
-  //     context: context,
-  //     position:
-  //         position, // Adjust the position according to your icon's position
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: TextField(
+          onChanged: (searchTerm) {
+            // _performSearch(searchTerm);
+          },
+          cursorColor: ColorConstant.primary,
+          decoration: InputDecoration(
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: ColorConstant.paleblue),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: ColorConstant.paleblue),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            hintText: "Search",
+            filled: false,
+            fillColor: const Color(0xfff1f1f1),
+            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: ColorConstant.paleblue),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: Colors.grey),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            suffixIcon: Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 12,
+      ),
+      Expanded(
+        child: SizedBox(
+          height: 300,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            elevation: 3,
+            shadowColor: Colors.black,
+            clipBehavior: Clip.antiAlias,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('assets/cafe1.png'),
+                  SizedBox(
+                      // width: 8,
+                      ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(' hi',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(color: ColorConstant.primary)),
+                      Text('hi',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  color: ColorConstant.paleblue))
+                    ],
+                  ),
+                  Spacer(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Reward Points',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: ColorConstant.paleblue)),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/coin.png',
+                            alignment: Alignment.center,
+                          ),
+                          // SizedBox(
+                          //   width: width * 0.01,
+                          // ),
+                          Text('hi',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(color: ColorConstant.primary))
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
 
-  //     color: Colors.white,
-  //     items: [
-  //       const PopupMenuItem<String>(
-  //         value: 'Recent',
-  //         child: Text('Recent'),
-  //       ),
-  //       const PopupMenuItem<String>(
-  //         value: 'Alphabetical',
-  //         child: Text('Alphabetical'),
-  //       ),
-  //     ],
-  //     initialValue: _selectedOption, // The initial selected value
-  //   );
+class RecentDataWidget extends StatelessWidget {
+  const RecentDataWidget({super.key});
 
-  //   if (selectedOption != null && selectedOption != _selectedOption) {
-  //     setState(() {
-  //       _selectedOption = selectedOption;
-  //       _fetchData();
-  //     });
-  //   }
-  // }
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobileowner/auth/signup/pages/signup.dart';
@@ -32,3 +30,74 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   bool showRecentData = false;
+
+//   void toggleData() {
+//     setState(() {
+//       showRecentData = !showRecentData;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Conditional Data Example'),
+//       ),
+//       body: Column(
+//         children: [
+//           ElevatedButton(
+//             onPressed: toggleData,
+//             child: Text('Toggle Data'),
+//           ),
+//           if (showRecentData) RecentDataWidget() else NextDataWidget(),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class RecentDataWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text(
+//         'Recent Data',
+//         style: TextStyle(fontSize: 24.0),
+//       ),
+//     );
+//   }
+// }
+
+// class NextDataWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text(
+//         'Next Data',
+//         style: TextStyle(fontSize: 24.0),
+//       ),
+//     );
+//   }
+// }
